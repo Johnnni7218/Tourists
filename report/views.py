ee071514d849cde9df9d6470d61aa1cd809d151f
Johnnni7218 <johnnni@mail.ru>
2023-09-07 21:21:40 +0300

    add views and urls

--- report/views.py
+++ report/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework import permissions
 
-# Create your views here.
+from .serializers import *
+from .models import *
+
+
+class UserViewset(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class CoordsViewset(viewsets.ModelViewSet):
+    queryset = Coords.objects.all()
+    serializer_class = CoordsSerializer
+
+
+class MountViewset(viewsets.ModelViewSet):
+    queryset = Mount.objects.all()
+    serializer_class = MountSerializer
+
+
+class PhotoViewset(viewsets.ModelViewSet):
+    queryset = Photo.objects.all()
+    serializer_class = PhotoSerializer
+
